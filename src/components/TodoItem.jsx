78d191acfac79b8626ba717161e9f2dfa98d5f33
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2023-01-29 17:29:37 -0500

    to do list

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,6 +6,25 @@ const ItemText = styled.span`
   text-decoration: ${(props) => (props.isCompleted ? "line-through" : "none")};
 `;
 
+const ListItem = styled.li`
+  list-style: none; /* remove the bullet points */
+  margin: 10px 0; /* add some margin between each list item */
+  padding: 10px; /* add some padding to the list item */
+  background-color: #f2f2f2; /* change the background color */
+  border-radius: 5px; /* add some border radius to give it a rounded edge */
+`;
+
+const CloseButton = styled.button`
+  font-size: 16px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  color: #bbb;
+  &:hover {
+    color: #555;
+  }
+`;
+
 function ToDoItem(props) {
   const [isCompleted, setIsCompleted] = useState(props.isCompleted);
 
@@ -14,10 +33,11 @@ function ToDoItem(props) {
   };
 
   return (
-    <div>
+    <ListItem>
       <input type="checkbox" checked={isCompleted} onChange={handleCheckBox} />
       <ItemText isCompleted={isCompleted}>{props.item}</ItemText>
-    </div>
+      <CloseButton onClick={props.handleRemove}>x</CloseButton>
+    </ListItem>
   );
 }
 
--- src/containers/ToDo.jsx
+++ src/containers/ToDo.jsx
@@ -1,5 +1,28 @@
 import React, { useState } from "react";
 import ToDoItem from "../components/TodoItem";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #e2d5de;
+  height: 100vh;
+`;
+
+const ToDoContainer = styled.div`
+  padding: 3rem !important;
+  background-color: white;
+  width: 50%;
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  border-radius: 15px;
+  font-family: Roboto, sans-serif;
+  padding: 48px;
+`;
+
+const CardBody = styled.div``;
 
 function ToDo() {
   const [items, setItems] = useState([]);
@@ -11,22 +34,52 @@ function ToDo() {
     setNewItem("");
   }
 
+  function removeItem(index) {
+    const newItems = [...items];
+    newItems.splice(index, 1);
+    setItems(newItems);
+  }
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
-        />
-        <button type="submit">Add Item</button>
-      </form>
-      <ul>
-        {items.map((item, index) => (
-          <ToDoItem item={item.item} isCompleted={item.isCompleted} />
-        ))}
-      </ul>
-    </div>
+    <Container>
+      <ToDoContainer>
+        <CardBody>
+          <h4 className="mb-3">To Do List</h4>
+          <form
+            onSubmit={handleSubmit}
+            className="d-flex justify-content-center align-items-center mb-4"
+          >
+            <div className="form-outline flex-fill">
+              <div className="input-group">
+                <div className="form-label">
+                  <label htmlFor="form3"></label>
+                </div>
+                <input
+                  id="form3"
+                  type="text"
+                  placeholder="What do you need to do today?"
+                  value={newItem}
+                  onChange={(e) => setNewItem(e.target.value)}
+                />
+                <button type="submit" className="btn btn-primary btn-lg ms-2">
+                  Add
+                </button>
+              </div>
+            </div>
+          </form>
+          <ul>
+            {items.map((item, index) => (
+              <ToDoItem
+                item={item.item}
+                isCompleted={item.isCompleted}
+                index={index}
+                handleRemove={() => removeItem(index)}
+              />
+            ))}
+          </ul>
+        </CardBody>
+      </ToDoContainer>
+    </Container>
   );
 }
 
